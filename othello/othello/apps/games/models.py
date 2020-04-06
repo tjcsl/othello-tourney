35fef1e7349fd781043944daadd3978d3ab04f91
Anup Bagali <anup.bagali1@gmail.com>
2020-04-05 22:41:43 -0400

    add submission time to Submission model

--- othello/othello/apps/games/models.py
+++ othello/othello/apps/games/models.py
@@ -1,6 +1,15 @@
 import os
 from django.db import models
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+class OverwriteStorage(FileSystemStorage):
+
+    def get_available_name(self, name, **kwargs):
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return name
 
 
 class Submission(models.Model):
@@ -9,4 +18,5 @@ class Submission(models.Model):
         return os.path.join(self.user.short_name, "strategy.py")
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user")
-    code = models.FileField(upload_to=upload_path)
+    submitted_time = models.DateTimeField(auto_now=True)
+    code = models.FileField(upload_to=upload_path, storage=OverwriteStorage())
