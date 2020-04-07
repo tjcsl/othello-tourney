99c8fc888cfdff297c6aa8b68c4c7393a1b8c5d7
Anup Bagali <anup.bagali1@gmail.com>
2020-04-07 03:10:01 -0400

    SubmissionForm: use ModelChoiceForm so we can query Submission model on load-time

--- othello/othello/apps/games/forms.py
+++ othello/othello/apps/games/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Submission, Game
+from .models import Submission
 
 
 class SubmissionForm(forms.ModelForm):
@@ -10,7 +10,7 @@ class SubmissionForm(forms.ModelForm):
 
 
 class GameForm(forms.Form):
-    choices = [(submission.id, submission.user) for submission in Submission.objects.order_by('user', '-submitted_time').distinct('user')]
-    black = forms.ChoiceField(label="Black:", choices=choices, initial="Yourself")
-    white = forms.ChoiceField(label="White:", choices=choices, initial="Yourself")
+    choices = Submission.objects.order_by('user', '-submitted_time').distinct('user')
+    black = forms.ModelChoiceField(label="Black:", queryset=choices, initial="Yourself")
+    white = forms.ModelChoiceField(label="White:", queryset=choices, initial="Yourself")
     time_limit = forms.IntegerField(label="Time Limit (secs):", initial=5, min_value=1)
