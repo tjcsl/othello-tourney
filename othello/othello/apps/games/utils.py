d95fffdc26b70d43e2a655d0c9e0e95db96dd91b
Anup Bagali <anup.bagali1@gmail.com>
2020-04-09 00:20:42 -0400

    return function after assertion

--- othello/othello/apps/games/utils.py
+++ othello/othello/apps/games/utils.py
@@ -6,3 +6,4 @@ from inspect import signature
 
 def import_strategy(path):
     assert len(signature(func := importlib.machinery.SourceFileLoader("strategy", path, ).load_module().Strategy().best_strategy).parameters) == 4
+    return func
