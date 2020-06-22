d69c4ca013599d79830087fe0bba501287ca381c
Anup Bagali <anup.bagali1@gmail.com>
2020-06-22 02:41:16 -0400

    add django_extensions

--- othello/settings/__init__.py
+++ othello/settings/__init__.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "channels",
+    "django_extensions",
     "social_django",
     "django_celery_results",
     "othello.apps",
