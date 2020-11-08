1cac8b98680482a63c5b1be0f53ec8e2a33627e1
Anup Bagali <anup.bagali1@gmail.com>
2020-11-07 21:13:05 -0500

    Update utils.py

--- othello/apps/tournaments/utils.py
+++ othello/apps/tournaments/utils.py
@@ -20,7 +20,7 @@ def make_pairings(
     players = sorted(players, key=lambda x: -x.ranking)
 
     for i in range(0, len(players), 2):
-        if i + 1 > len(players):
+        if i + 1 >= len(players):
             players.append(bye_player)
         black = random.choice((players[i], players[i + 1]))
         white = players[i] if black == players[i + 1] else players[i + 1]
