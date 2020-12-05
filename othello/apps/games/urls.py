d8b4a6d9d4850412ca91020a2a4e4e2c742b649c
Anup Bagali <anup.bagali1@gmail.com>
2020-12-04 20:45:38 -0500

    frontend changes

--- othello/apps/games/urls.py
+++ othello/apps/games/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path("download/", views.download, name="download"),
     path("watch/", views.watch, name="watch"),
     path("watch/<int:game_id>", views.watch, name="watch"),
-    path("about/", views.about, name="about"),
     path("help/", views.help_view, name="help"),
 ]
