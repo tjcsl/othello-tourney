184f1283a4ba17772001f23b5b3112c222082e49
Anup Bagali <anup.bagali1@gmail.com>
2021-04-22 11:29:02 -0400

    emails: add bcc option

--- othello/apps/tournaments/emails.py
+++ othello/apps/tournaments/emails.py
@@ -6,12 +6,15 @@ from django.template.loader import get_template
 
 
 def email_send(
-    text_template: str, html_template: str, data: Dict[str, Any], subject: str, emails: List[str]
+    text_template: str, html_template: str, data: Dict[str, Any], subject: str, emails: List[str], bcc: bool = False
 ) -> EmailMultiAlternatives:
     text, html = get_template(text_template), get_template(html_template)
     text_content, html_content = text.render(data), html.render(data)
     subject = settings.EMAIL_SUBJECT_PREFIX + subject
-    msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_FROM, emails, headers={})
+    if bcc:
+        msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_FROM, [], headers={}, bcc=emails)
+    else:
+        msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_FROM, emails, headers={})
     msg.attach_alternative(html_content, "text/html")
 
     if not settings.DEBUG or settings.FORCE_EMAIL_SEND:
