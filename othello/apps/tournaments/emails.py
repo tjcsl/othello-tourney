fe45dd13d7a5892da0ae73906943083ee8e79c47
abagali1 <anup.bagali1@gmail.com>
2021-04-22 12:41:13 -0400

    tournaments: start notification emails

--- othello/apps/tournaments/emails.py
+++ othello/apps/tournaments/emails.py
@@ -6,13 +6,20 @@ from django.template.loader import get_template
 
 
 def email_send(
-    text_template: str, html_template: str, data: Dict[str, Any], subject: str, emails: List[str], bcc: bool = False
+    text_template: str,
+    html_template: str,
+    data: Dict[str, Any],
+    subject: str,
+    emails: List[str],
+    bcc: bool = False,
 ) -> EmailMultiAlternatives:
     text, html = get_template(text_template), get_template(html_template)
     text_content, html_content = text.render(data), html.render(data)
     subject = settings.EMAIL_SUBJECT_PREFIX + subject
     if bcc:
-        msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_FROM, [], headers={}, bcc=emails)
+        msg = EmailMultiAlternatives(
+            subject, text_content, settings.EMAIL_FROM, [], headers={}, bcc=emails
+        )
     else:
         msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_FROM, emails, headers={})
     msg.attach_alternative(html_content, "text/html")
