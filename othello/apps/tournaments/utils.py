161df364015f810ac5b8585a91db5d1e5a564182
abagali1 <anup.bagali1@gmail.com>
2021-04-22 22:47:16 -0400

    tournaments: rankings are not updated every round

--- othello/apps/tournaments/utils.py
+++ othello/apps/tournaments/utils.py
@@ -1,9 +1,11 @@
 import random
+import logging
 from typing import Generator, List, Tuple, TypeVar
 
 from .models import TournamentPlayer
 
 T = TypeVar("T")
+logger = logging.getLogger("othello")
 
 
 def chunks(v: List[T], n: int) -> Generator[List[T], None, None]:
@@ -18,6 +20,7 @@ def make_pairings(players: List[TournamentPlayer], bye_player: TournamentPlayer)
     for i in range(0, len(players), 2):
         if i + 1 >= len(players):
             players.append(bye_player)
+        logger.info(f"RANK: {players[i]}({players[i].ranking}), {players[i+1]}({players[i+1].ranking})")
         black = random.choice((players[i], players[i + 1]))
         white = players[i] if black == players[i + 1] else players[i + 1]
         matches.append((black, white))
